f61ef0d10ef19b833ec418d9ef741a32e180fa96
Olmeken <116932@vitusmavo.nl>
2022-04-21 20:38:03 +0200

    Revert "Update layers.js"
    This reverts commit 924fe442857e00eb7d607021b875063754b97555.

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("l", {
-    name: "Advenute Capitalist", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "AdCap", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("p", {
+    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "Yellow",
+    color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "points", // Name of prestige currency
+    resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -22,14 +22,9 @@ addLayer("l", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "a", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true},
-    upgrades: {
-        11: {
-
-        },
-    },
+    layerShown(){return true}
 })
 
     
