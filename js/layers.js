f3b3945f92543fca5b1b83365b4a80802d07e214
Olmeken <116932@vitusmavo.nl>
2022-04-21 20:20:23 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,12 +1,12 @@
-addLayer("l", {
-    name: "Lemons", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "L", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("p", {
+    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "Yellow",
+    color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -26,4 +26,6 @@ addLayer("l", {
     ],
     layerShown(){return true}
 })
+
+    
 
