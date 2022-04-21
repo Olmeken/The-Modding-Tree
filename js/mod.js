52730779c28b6b2950f75313bd4802911c4e283b
Olmeken <116932@vitusmavo.nl>
2022-04-21 20:55:51 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The no Tree",
+	name: "Adventure Capitalist Tree",
 	id: "chickem",
-	author: "nobody",
+	author: "kip",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -41,7 +41,7 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-
+		if (hasUpgrade('p', 11)) gain = gain.times(2)
 	let gain = new Decimal(1)
 	return gain
 }
